b3ce40c640620c840e9608dfcea9ce8bb73a892c
RITAP28 <ritap2804manutd@gmail.com>
2025-07-12 13:13:45 +0530

    token middleware added

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { register } from '../controllers/auth/user.register';
 import { login } from '../controllers/auth/user.login';
 import { logout } from '../controllers/auth/user.logout';
+import { authenticateUser } from '../middlewares/token.middleware';
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/logout').post(logout);
+router.route('/logout/:userId').post(authenticateUser, logout);
 
 export default router;
